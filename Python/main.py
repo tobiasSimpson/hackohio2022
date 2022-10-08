7c4831f5eef1241e903a0d180f25d866bc69dd71
tobiasSimpson <93829921+tobiasSimpson@users.noreply.github.com>
2022-10-08 12:29:45 -0400

    Initial data procesing

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,22 @@
 import pandas as pd
 
-df = pd.read_csv('Dorm Buildings.csv')
+def regression(dfDorm):
+    return 0
+
+def leaderboard(dfAll,dfDorm):
+    return 0
+
+def graph(dfDorm):
+    return 0
+
+#Name of the display dorm
+dormName="Busch House"
+#Read complete dataset
+dfAll = pd.read_csv("Python\Dorm Buildings.csv")
+
+#Create a datasubset for specific dorm
+dormNameCols = dfAll.columns
+dfDorm = dfAll[["Series Name"]]
+for x in dormNameCols:
+    if dormName in x:
+        dfDorm = pd.concat([dfDorm,dfAll[[x]]])
